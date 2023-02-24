86b3f48a53b53e0ed155443c41a9768a11c24f71
Nguyen Nhut <nguyen.nhut.99.2017@gmail.com>
2023-02-24 19:45:52 +0700

    update admin

--- server/routes/api.php
+++ server/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AdminController;
+
  
 
 /*
@@ -30,4 +32,5 @@ Route::get('/category/{id}',[CategoryController::class,'show']);
 Route::get('/product',[ProductController::class,'index']);
 Route::get('/product/{id}',[ProductController::class,'show']);
 
-
+Route::get('/admin/{id}', [AdminController::class, 'show']);
+Route::get('/admin', [AdminController::class, 'index']);
