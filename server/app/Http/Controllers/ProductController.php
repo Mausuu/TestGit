242c701de96980e3f2eda52c8835a4594089ac91
noobnhut <nguyen.nhut.99.2017@gmail.com>
2023-03-16 08:35:18 +0700

    a

--- server/app/Http/Controllers/ProductController.php
+++ server/app/Http/Controllers/ProductController.php
@@ -40,7 +40,7 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request  
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
